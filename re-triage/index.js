a727e843072943a3e83b2fb3c87862e1220bf248
Vera Reynolds <vreynolds@users.noreply.github.com>
2021-12-28 12:50:52 -0700

    re-triage archived cards (#16)

--- re-triage/index.js
+++ re-triage/index.js
@@ -7,16 +7,30 @@ void async function () {
 	try {
 		const iceboxColumn = 12117642;
 		const triageColumn = 14962835;
+		const doneColumn = 12315669;
 		const issueUrl = github.context.payload.issue.url;
 		const issueOpen = github.context.payload.issue.state === "open";
+		const closedAt = github.context.payload.issue.closed_at;
+
+		// don't re-triage closing comments
+		if (!issueOpen) {
+			let now = new Date();
+			let closedTime = new Date(closedAt);
+			let closedMinutesAgo = (now - closedTime) / 60000;
+			if (closedMinutesAgo < 10) {
+				console.log('likely a closing comment, bailing');
+				return;
+			}
+		}
+
 		const accessToken = core.getInput('ghprojects-token');
 		const octokit = github.getOctokit(accessToken);
 		let page = 1;
 		let issueCard;
 		while (!issueCard) {
 			let response = await octokit.rest.projects.listCards({
-				column_id: iceboxColumn,
-				archived_state: issueOpen ? "not_archived" : "archived",
+				column_id: issueOpen ? iceboxColumn : doneColumn,
+				archived_state: issueOpen ? "not_archived" : "all",
 				per_page: 100,
 				page: page
 			});
@@ -35,7 +49,15 @@ void async function () {
 				page++;
 			}
 		}
+
 		if (issueCard) {
+			// archived cards need to be un-archived first
+			if (!issueOpen) {
+				await octokit.rest.projects.updateCard({
+					card_id: issueCard.id,
+					archived: false
+				});
+			}
 			await octokit.rest.projects.moveCard({
 				card_id: issueCard.id,
 				column_id: triageColumn,
